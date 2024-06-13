aa1d18efb89cb189dad2d75312dd777a3292b25a
sukiratwarring <sukiratwarring@gmail.com>
2024-06-13 18:15:34 +0530

    Anchor_staking_contract

---
+++ tests/checking.ts
@@ -0,0 +1,18 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Checking } from "../target/types/checking";
+
+describe("checking", () => {
+  console.log("->>>>>>>>>>>>>>>>>>>>>");
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Checking as Program<Checking>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
